f84d2ba1c9bd14335bd0e6b6622aa25c7ac71487
sharadj34 <sharad.jha1010@gmail.com>
2023-10-05 15:47:31 +0530

    updated

---
+++ 001_basics.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //print hello world
+    cout<<"Hello World";
+
+    //data types
+    int a = 123;
+    cout << a << endl;
+
+    char b = 'g';
+    cout << b << endl;
+
+    bool c = true;
+    cout << c << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.23;
+    cout << d << endl;
+
+    int size = sizeof(d);
+    cout <<"Size of d is : " << size <<endl;
+
+    return 0;
+}
