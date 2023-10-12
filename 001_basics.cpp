0bebe8926c8fd18507d6d9d88d7547314f7cae99
sharadj34 <sharad.jha1010@gmail.com>
2023-10-12 18:02:35 +0530

    updated basics

--- 001_basics.cpp
+++ 001_basics.cpp
@@ -1,4 +1,3 @@
-
 *** print hello world ***
 
 #include <iostream>
@@ -66,3 +65,95 @@ int main()
     cout<<str;
     return 0;
 }
+
+
+*** #define preprocessor ***
+#include <iostream>
+using namespace std;
+#define length 10
+#define width 15
+#define newline '\n'
+int main()
+{
+    int area = length * width;
+    cout << area;
+    cout << newline;
+    return 0;
+}
+
+
+*** Using const keyword ***
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int length = 10;
+    const int width = 15;
+    const char newline = '\n';
+    int area = length * width;
+    cout << area;
+    cout << newline;
+    return 0;
+}
+
+
+*** if-else-if ***
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cin >> marks;
+    if(marks < 25)
+    cout << "Grade: F";
+    else if(marks>=25 && marks<=44)
+    cout << "Grade: E";
+    else if(marks>=45 && marks<=49)
+    cout << "Grade: D";
+    else if(marks>=50 && marks<=59)
+    cout << "Grade: C";
+    else if(marks>=60 && marks<=79)
+    cout << "Grade: B";
+    else if(marks>=80 && marks<=100)
+    cout << "Grade: A";
+    return 0;
+}
+
+
+*** Switch Case ***
+#include <iostream>
+using namespace std;
+int main()
+{
+    int day;
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout<<"Monday";
+        break;
+    case 2:
+        cout<<"Tuesday";
+        break;
+    case 3:
+        cout<<"Wednesday";
+        break;
+    case 4:
+        cout<<"Thursday";
+        break;
+    case 5:
+        cout<<"Friday";
+        break;
+    case 6:
+        cout<<"Saturday";
+        break;
+    case 7:
+        cout<<"Sunday";
+        break;
+    default:
+        cout<<"Not a day";
+        break;
+    }
+    return 0;
+}
+
