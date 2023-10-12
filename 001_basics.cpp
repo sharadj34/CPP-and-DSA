3725b0180e0b1d607b0d04c143d0746c237a1cad
sharadj34 <sharad.jha1010@gmail.com>
2023-10-12 16:34:44 +0530

    updated

--- 001_basics.cpp
+++ 001_basics.cpp
@@ -1,13 +1,29 @@
+
+*** print hello world ***
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    //print hello world
     cout<<"Hello World";
+    return 0;
+}
 
-    //data types
+
+*** data types ***
+
+#include <iostream>
+using namespace std;
+int main()
+{
     int a = 123;
     cout << a << endl;
+
+    long e = 435543;
+    cout<< e << endl;
+
+    long long g = 434325436;
+    cout<< g << endl;
 
     char b = 'g';
     cout << b << endl;
@@ -21,8 +37,32 @@ int main()
     double d = 1.23;
     cout << d << endl;
 
-    int size = sizeof(d);
-    cout <<"Size of d is : " << size <<endl;
+    return 0;
+}
 
+
+*** sizeof function ***
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //sizeof
+    cout << "size of int is: " << sizeof(int) << endl;
+    cout << "size of char is: " << sizeof(char) << endl;
+    cout << "size of double is: " << sizeof(double) << endl;
+
+    return 0;
+}
+
+
+*** getline function for string ***
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin,str);
+    cout<<str;
     return 0;
 }
