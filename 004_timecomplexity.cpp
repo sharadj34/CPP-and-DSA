396f60c771042ef06f91900055c615958e7d8617
sharadj34 <sharad.jha1010@gmail.com>
2023-10-16 16:21:46 +0530

    updated

--- 004_timecomplexity.cpp
+++ 004_timecomplexity.cpp
@@ -1,3 +1,15 @@
+//Rate at which the time taken increases with respect to the input size is known as time complexity.
+
+// Every piece of code takes time in terms of Big-O Notaion.
+
+/*
+The 3 rules to write time complexity are: 
+1) Compute time complexity in terms of worst case scenario.
+2) Avoid constants
+3) Avoid lower values
+*/ 
+
+
 // QUESTION 1
 for (int i = 0; i < N; i++)
 {
