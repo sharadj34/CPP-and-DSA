1a0f9dfbc5cbe46899ee3bbeea26c6af9a3fc0b0
sharadj34 <sharad.jha1010@gmail.com>
2023-10-27 18:13:50 +0530

    updated

--- 004_STL.cpp
+++ 004_STL.cpp
@@ -144,11 +144,122 @@ void explainStack()
     st1.swap(st2);
 }
 
+
+void explainQueue() 
+{
+    queue<int> q;
+    q. push(1); // (1)
+    q. push(2); // (1, 2)
+    q. emplace(4); // (1, 2, 4)
+    
+    q.back() += 5;
+    
+    cout << q.back(); // prints 9
+    
+    // Q is {1, 2, 9}
+    cout << q. front(); // prints 1
+    
+    q.pop(); // {2, 9)
+    
+    cout << q.front(); // prints 2
+    // size swap empty same as stack
+}
+
+
+void explainPQ()
+{
+    priority_queue<int> pq;
+    pq.push(5); // {5}
+    pq.push(2); // {5,2}
+    pq.push(8); // {8,5,2}
+    pq.emplace(10); // {10,8,5,2}
+
+    cout<<pq.top(); // prints 10
+
+    pq.pop(); // {8,5,2}
+
+    cout << pq.top(); // prints 8
+
+    // size, swap, empty function same as others
+
+    // Minimum Heap
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(5); // {5}
+    pq.push(2); // {2,5}
+    pq.push(8); // {2,5,8}
+    pq.emplace(10); // {2,5,8,10}
+
+    cout << pq.top(); // prints 2
+}
+
+
+void explainSet()
+{
+    // It stores everything in the sorted order and unique
+    set<int> st;
+    st.insert(1); // {1}
+    st.emplace(2); // {1,2}
+    st.insert(2); // {1,2}
+    st.insert(4); // {1,2,4}
+    st.insert(3); // {1,2,3,4}
+
+    // Functionality of insert can be also used in vector also, that only increases efficiency
+    // begin() , end() , rbegin() , rend() , size() , empty() and swap() are same as those of above
+
+    // {1,2,3,4,5}
+    auto it = st.find(3);
+
+    // {1,2,3,4,5}
+    auto it = st.find(6); // If an element is not there in the set it always returns st.end()
+
+    // {1,2,3,4}
+    st.erase(5); // erases 5, it takes logarithmic time
+
+    int cnt = st.count(1); // if 1 exists in the set it will return 1 otherwise 0
+
+    auto it = st.find(3);
+    st.erase(it); // it takes constant time
+
+    //{1,2,3,4,5}
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1,it2); // after erase {1,4,5} [first, last)
+
+    auto it = st.lower_bound(2);
+    auto it = st.upper_bound(3);
+}
+
+
+void explainMultiSet()
+{
+    // Everything is same as set, only stores duplicate elements also
+
+    multiset<int> ms;
+    ms.insert(1); // {1}
+    ms.insert(1); // {1,1}
+    ms.insert(1); // {1,1,1}
+
+    ms.erase(1); // all 1's erased
+
+    int cnt = ms.count(1); 
+
+    ms.erase(ms.find(1)); // only a single one erased
+
+    ms.erase(ms.find(1), ms.find(1+2));
+
+    // rest all functions are same as set
+}
+
 int main()
 {
     explainPair();
     explainVector();
     explainList();
     explainDeque();
+    explainStack();
+    explainQueue();
+    explainPQ();
+    explainSet();
+    explainMultiSet();
     return 0;
 }
