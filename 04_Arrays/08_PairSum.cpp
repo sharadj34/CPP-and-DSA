502fa29bf9f6b221fce900ff689408ef4f286f83
sharadj34 <sharad.jha1010@gmail.com>
2023-11-02 23:08:27 +0530

    updated

---
+++ 04_Arrays/08_PairSum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   // Write your code here.
+   vector <vector <int>> ans;
+
+   for(int i=0 ; i<arr.size() ; i++)
+   {
+      for(int j=i+1 ; j<arr.size() ; j++)
+      {
+         if(arr[i] + arr[j] == s)
+         {
+         vector <int> temp;
+         temp.push_back(min(arr[i] , arr[j]));
+         temp.push_back(max(arr[i] , arr[j]));
+         ans.push_back(temp);
+         }
+      }
+   }
+   sort(ans.begin() , ans.end());
+   return ans;
+}
+
+int main()
+{
+    int n, S;
+    cin >> n >> S;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<vector<int>> pairs = pairSum(arr, S);
+    for (auto pair : pairs)
+    {
+        cout << pair[0] << " " << pair[1] << endl;
+    }
+
+    
+    return 0;
+}
