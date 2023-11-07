880021b4ff53c146de7d79a9caa32f364f77b0df
sharadj34 <sharad.jha1010@gmail.com>
2023-11-07 23:53:49 +0530

    updated

--- Binary Search/006_sqrt_using_BS.cpp
+++ Binary Search/006_sqrt_using_BS.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int mySqrt(int x) {
 
-        if (x == 0 || x == 1)
-            return x;
-        
-        int start = 1;
-        int end = x;
-        int mid = -1;
-        
-        while (start <= end) {
+long long int binarysearch(int n)
+{
+    int s = 0;
+    int e = n;
+    long long int mid = s + (e-s)/2;
+    long long int ans = -1;
 
-            mid = start + (end - start) / 2;
-            
-            long long square = static_cast<long long>(mid) * mid;
- 
-            if (square > x)
-                end = mid - 1;
-            else if (square == x)
-                return mid; 
-            else
-    
-                start = mid + 1;
+    while(s<=e)
+    {
+        long long int square = mid * mid;
+
+        if(square == n)
+        return mid;
+        if(square < n)
+        {
+            ans = mid;
+            s = mid+1;
         }
-        return static_cast<int>(std::round(end));
-
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+    int mySqrt(int x) {
+        return binarysearch(x);
     }
 };
