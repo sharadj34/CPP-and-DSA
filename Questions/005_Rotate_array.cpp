8ee00e50366ac0df60a472494d628ca9454fdc2d
sharadj34 <sharad.jha1010@gmail.com>
2023-11-14 21:38:02 +0530

    updated

---
+++ Questions/005_Rotate_array.cpp
@@ -0,0 +1,20 @@
+/*
+QUESTION: Rotate Array
+
+LINK: https://leetcode.com/problems/rotate-array/
+*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+       int n = nums.size();
+       vector<int> temp(n);
+
+       for(int i=0 ; i<n ; i++)
+       {
+           temp[(i+k)%n] = nums[i]; 
+       }
+       nums = temp; 
+    }
+};
