f76969c10e9201a382b8907aa719678466c5f3f4
sharadj34 <sharad.jha1010@gmail.com>
2023-11-15 20:25:34 +0530

    updated

--- Questions/007_sum_of_two_arrays.cpp
+++ Questions/007_sum_of_two_arrays.cpp
@@ -4,28 +4,40 @@ QUESTION: Sum of two arrays
 LINK: https://www.codingninjas.com/studio/problems/sum-of-two-arrays_893186
 */
 
-#include <bits/stdc++.h> 
-vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) 
-{
-    string k , l;
-    for(int i = 0; i < n;i++)
-    {
-        k += to_string(a[i]);
-    }
-
-    for(int i = 0;i < m;i++)
-    {
-        l += to_string(b[i]);
-    }
-
-    int num = stoi(k) + stoi(l);
-    string res = to_string(num);
-
-    vector<int>v;
-    for(int j = 0;j < res.size();j++)
-    {
-        int ans = res[j] - '0';
-        v.push_back(ans);
-    } 
-    return v; 
+#include <bits/stdc++.h>
+vector<int> findArraySum(vector<int>a, int n, vector<int>b, int m) {
+    
+	vector<int>ans;
+	int carry = 0;
+	int sum = 0;
+	int i = n - 1;
+    int j = m - 1;
+    
+	while (i >= 0 || j >= 0) {
+		sum = 0;
+        // If we have some elements left in the first array, then add it to the sum.
+		if (i >= 0) {
+			sum += a[i];
+			i--;
+		}
+        
+        // If we have some elements left in the second array, then add it to the sum.
+		if (j >= 0) {
+			sum += b[j];
+			j--;
+		}
+        
+		sum += carry;
+		int lastDigit = sum % 10;
+		carry = sum / 10;
+		ans.push_back(lastDigit);
+	}
+    
+    // If still some carry is left, then push it to the answer.
+	if (carry) {
+		ans.push_back(carry);
+	}
+    
+	reverse(ans.begin(), ans.end());
+	return ans;
 }
