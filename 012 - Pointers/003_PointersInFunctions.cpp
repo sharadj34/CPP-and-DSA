d53d740da66ab879e58bec345f638d586b4ba279
sharadj34 <sharad.jha1010@gmail.com>
2023-11-26 23:46:39 +0530

    updated

--- 012 - Pointers/003_PointersInFunctions.cpp
+++ 012 - Pointers/003_PointersInFunctions.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void print(int *p)
+{
+    cout << *p << endl;
+}
+
+void update(int *p)
+{
+    *p = *p+1;
+}
+
+int getSum(int arr[], int n) // int *arr == int arr[] (pointer is passed instead of the whole array)
+{
+    cout << "Size of array: " << sizeof(arr) << endl;
+    int sum = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
 int main()
 {
+    int value = 5;
+    int *p = &value;
+
+    print(p);
+    cout << "Address Before: " << p << endl;
+    cout << "Value Before: " << *p << endl;
+
+    update(p);
+
+    cout << "Adress After: " << p << endl;
+    cout << "Value After: " << *p << endl;
+
+
+    int arr[5] = {1,2,3,4,5};
+    cout << "Sum is : " << getSum(arr, 5) << endl;
 
     return 0;
 }
