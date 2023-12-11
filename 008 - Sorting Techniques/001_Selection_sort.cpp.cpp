9acc8d1b4a6ffb1d82606a0e131a6e724d95a626
sharadj34 <sharad.jha1010@gmail.com>
2023-12-11 15:47:55 +0530

    updated

--- 008 - Sorting Techniques/001_Selection_sort.cpp.cpp
+++ 008 - Sorting Techniques/001_Selection_sort.cpp.cpp
@@ -10,14 +10,11 @@ void selectionSort(int arr[], int n)
 {   
     for(int i=0 ; i<n-1 ;i++)
     {
-        int minIndex = i;
-
         for(int j = i+1 ; j<n ; j++)
         {
-            if(arr[j] < arr[minIndex])
-            minIndex = j;
+            if(arr[j] < arr[i])
+            swap(arr[j] , arr[i]);
         }
-        swap(arr[minIndex] , arr[i]);
     }
 }
 
