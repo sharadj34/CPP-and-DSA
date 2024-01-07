7c46bc5ae30afeeab5fc811213d294a5a6ae00c1
sharadj34 <sharad.jha1010@gmail.com>
2024-01-07 22:12:16 +0530

    updated

---
+++ 015 - Linked List/Introduction.cpp
@@ -0,0 +1,13 @@
+/*
+
+What is Linked List?
+-> A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations. The elements in a linked list are linked using pointers.
+
+Why Linked List?
+-> Dynamic data structure
+-> Grow/Shrink at runtime
+-> No memory wastage
+
+*/
+
+
--- 015 - OOPs Concepts/001_Introduction.cpp
+++
@@ -1,1 +0,0 @@
-// OOPs refers to languages that use objects in programming, they use objects as a primary source to implement what is to happen in the code.
