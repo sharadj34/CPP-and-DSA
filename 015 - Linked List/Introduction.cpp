d282a955ac6442653162592c079e10653b1b0a81
sharadj34 <sharad.jha1010@gmail.com>
2024-01-08 20:45:23 +0530

    updated

--- 015 - Linked List/Introduction.cpp
+++ 015 - Linked List/Introduction.cpp
@@ -11,3 +11,26 @@ Why Linked List?
 */
 
 
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+int main()
+{
+    Node* node1 = new Node(15);
+    cout << node1 -> data << endl;
+    cout << node1 -> next << endl;
+    return 0;
+}
